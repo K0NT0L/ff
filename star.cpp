fa206490275aeb2c66b626355a99c02c1aefc69e
K0NT0L <46758245+K0NT0L@users.noreply.github.com>
2019-03-31 08:39:30 +0700

    Update star.cpp

--- star.cpp
+++ star.cpp
@@ -25,3 +25,37 @@ cout << "Sudah Terupload:)";
 system("sh AdminSearch/install");
 }
 }
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+system("color 10");
+cout << "[=====================]\n";
+cout << "Author : r00t@star\n";
+cout << "powered by indonesian wibu :v\n";
+cout << "[=====================]\n\n";
+cout << "(1) RED_HAWK\n";
+cout << "(02) Tools By Pineaple_303\n";
+cout << "[======================]\n";
+int star;
+cout << "root@star -# ";
+cin >> star;
+if (star <= 1){
+cout << "Installing....";
+system("apt update && apt upgrade");
+system("apt install php");
+system("git clone https://github.com/Tuhinshubhra/RED_HAWK");
+cout << "cd RED_HAWK\n";
+cout << "Sudah Terupload :)\n";
+}else if (star <= 2){
+    cout << "Installing"
+    system("apt update && apt upgrade");
+    system("pkg install python2 lolcat git -y");
+    system("pip2 install lolcat");
+    system("git clone https://github.com/Xidploit/pinneapletools");
+    cout << "cd pinneapletools{enter}\n";
+    cout << "sudah terinstall :)\n";
+}
+}
