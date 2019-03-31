6967ab8e0c379b3e9745fd494e1017c99385c12d
K0NT0L <46758245+K0NT0L@users.noreply.github.com>
2019-03-31 08:43:05 +0700

    Update star.cpp

--- star.cpp
+++ star.cpp
@@ -1,31 +1,5 @@
 #include <iostream>
 
-using namespace std;
-
-int main()
-{
-system("color 10");
-cout << "[=====================]\n";
-cout << "Author : r00t@star\n";
-cout << "powered by indonesian wibu :v\n";
-cout << "[=====================]\n\n";
-cout << "(1) RED_HAWK\n";
-cout << ".\n";
-cout << "[======================]\n";
-int star;
-cout << "root@star -# ";
-cin >> star;
-if (star <= 1){
-cout << "Installing....";
-system("apt update && apt upgrade");
-system("apt install php");
-system("git clone https://github.com/Tuhinshubhra/RED_HAWK");
-cout << "cd RED_HAWK";
-cout << "Sudah Terupload:)";
-system("sh AdminSearch/install");
-}
-}
-#include <iostream>
 
 using namespace std;
 
